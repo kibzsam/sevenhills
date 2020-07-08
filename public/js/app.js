fe152b9e8f13f4aa26908f5649fcbb8189c81003
ireri brian <brian.ireri@students.dkut.ac.ke>
2020-07-08 22:49:28 +0300

    latest update

--- public/js/app.js
+++ public/js/app.js
@@ -2396,7 +2396,6 @@ __webpack_require__.r(__webpack_exports__);
 
       this.display = true;
       this.disabled = false;
-      this.$refs.signature.resizeCanvas();
     },
     generatePdf: function generatePdf() {
       var data = {
